deacea365af37462f9b0d2db99deed27abb0ec15
Dilshod Turobov <deebov@yandex.com>
2018-07-27 10:56:24 -0700

    add babel-loader, babel-polyfill, html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: ["babel-polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules : [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ],
+  }, 
+  devServer: {
+    contentBase: "./dist",
+    stats: "errors-only"
+  }
+};

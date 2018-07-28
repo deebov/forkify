570668c69980611ebfefb270cc4b9c70c422de53
Dilshod Turobov <deebov@yandex.com>
2018-07-28 04:40:57 -0700

    include the loader

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,10 +1,35 @@
+// DOM elements
 export const DOM = {};
 
-DOM.searchInput = $('.search__field');
-DOM.searchForm = $('.search');
-DOM.searchResList = $('.results__list');
+DOM.searchInput = $(".search__field");
+DOM.searchForm = $(".search");
+DOM.searchRes = $(".results");
+DOM.searchResList = $(".results__list");
+
+// DOM strings
+export const DOMStrings = {};
+
+DOMStrings.loader = "loader";
 
 // Function to select DOM elements
 function $(selector) {
   return document.querySelector(selector);
 }
+
+// Function to render the loader
+export const renderLoader = parent => {
+  const loader = `    
+          <div class="${DOMStrings.loader}">
+            <svg>
+                <use xlink:href="img/icons.svg#icon-cw"></use>
+            </svg>
+          </div>`;
+
+  parent.insertAdjacentHTML("afterbegin", loader);
+};
+
+export const clearLoader = parent => {
+  const loader = parent.querySelector(`.${DOMStrings.loader}`);
+
+  if (loader) parent.removeChild(loader);
+};

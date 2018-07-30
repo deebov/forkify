0fcb531eb9aa6894e40f4661af3b0da4e4e1ed9a
Dilshod Turobov <deebov@yandex.com>
2018-07-30 04:23:29 -0700

    add updateServings method

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -109,4 +109,16 @@ export default class Recipe {
     });
     this.ingredients = newIngredients;
   }
+
+  updateServings(type) {
+    // Servings
+    const newServings = type === 'dec' ? this.servings - 1 : type === 'inc' ? this.servings + 1 : this.servings;
+
+    // Ingredients
+    this.ingredients.forEach(ing => {
+      ing.count *= (newServings / this.servings);
+    });
+
+    this.servings = newServings;
+  }
 }

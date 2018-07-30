7f1588217aeb50a0d96af90f5497137a3fd01dd1
Dilshod Turobov <deebov@yandex.com>
2018-07-30 04:25:55 -0700

    update

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -6,11 +6,14 @@ DOM.searchForm = $('.search');
 DOM.searchRes = $('.results');
 DOM.searchResList = $('.results__list');
 DOM.searchResPages = $('.results__pages');
+DOM.recipe = $('.recipe');
 
 // DOM strings
 export const DOMStrings = {};
 
 DOMStrings.loader = 'loader';
+DOMStrings.searchLink = 'results__link';
+DOMStrings.searchLinkActive = 'results__link--active';
 
 // Function to select DOM elements
 function $(selector) {
